0ad85117f7c9227ef1673e9a2f89c41c087f28cd
kay <buh287@outlook.com>
2024-10-31 12:05:56 +0800

    window main.py
    just a window for the game i am gonna edit it more

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("playformer")
+
+BG_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 1000, 800
+
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+    clock = pygame.time.clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+              
+pygame.quit()
+quit()
+if _name_ == "_main_":
+    main(window)
